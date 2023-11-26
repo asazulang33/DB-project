032b3f1cd05a3cf5c4bba51b94928d8d7f60504c
김동회 <88483964+asazulang33@users.noreply.github.com>
2023-11-27 03:25:33 +0900

    Add files via upload

--- InitDb.java
+++ InitDb.java
@@ -44,18 +44,38 @@ public class InitDb {
             em.persist(location1);
             Location location2 = Location.createLoc("영화관", "수원시", "권선구", "금곡동 롯데시네마");
             em.persist(location2);
+            Location location3 = Location.createLoc("드라마 세트장", "서울시", "영등포구", "현대 백화점");
+            em.persist(location3);
+
 
             // 작품 생성 및 저장
-            createAndPersistArt("입시", 2018, Arrays.asList(actor1, actor2), location1);
-            createAndPersistArt("영화", 2020, Arrays.asList(actor2, actor3), location2);
-            createAndPersistArt("라면", 2018, Arrays.asList(actor3), location1);
+            createMovie("입시", 2018, Arrays.asList(actor1, actor2), location1);
+            createMovie("영화", 2020, Arrays.asList(actor2, actor3), location2);
+            createDrama("라면", 2018, Arrays.asList(actor3), location1);
+            createDrama("꽃보다 남자", 2016, Arrays.asList(actor1, actor2, actor3), location3);
         }
 
-        private void createAndPersistArt(String name, Integer year, List<Actor> actors, Location location) {
+        private void createMovie(String name, Integer year, List<Actor> actors, Location location) {
             Filmed filmed = Filmed.createFilmed(location);
             em.persist(filmed);
 
             Art art = Art.createArtMovie(name, year, filmed, actors);
+            em.persist(art);
+        }
+
+        private void createDrama(String name, Integer year, List<Actor> actors, Location location) {
+            Filmed filmed = Filmed.createFilmed(location);
+            em.persist(filmed);
+
+            Art art = Art.createArtDrama(name, year, filmed, actors);
+            em.persist(art);
+        }
+
+        private void createBook(String name, Integer year, List<Actor> actors, Location location) {
+            Filmed filmed = Filmed.createFilmed(location);
+            em.persist(filmed);
+
+            Art art = Art.createArtBook(name, year, filmed, actors);
             em.persist(art);
         }
 
