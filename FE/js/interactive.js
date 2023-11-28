703b821c9f5809bd57223c27fa79935a46dd3881
kim.B.G <kbg7553@ajou.ac.kr>
2023-11-29 00:29:55 +0900

    fix js error

--- FE/js/interactive.js
+++ FE/js/interactive.js
@@ -10,19 +10,19 @@ function handleOnChange(e) {
 }
 
 function toggleMenu(){
-	if{
+	if(document.getElementById("content-box-1").style.left == "0px") {
 		document.getElementById("content-box-1").style.left = "0px"
 	}
 	setTimeout(function(){
-		if{
+		if(document.getElementById("content-box-2").style.left == "0px") {
 			document.getElementById("content-box-2").style.left = "0px"
 		}
 		setTimeout(function(){
-			if{
+			if(document.getElementById("content-box-3").style.left == "0px") {
 				document.getElementById("content-box-3").style.left = "0px"
 			}
 			setTimeout(function(){
-				if{
+				if(document.getElementById("content-box-4").style.left == "0px") {
 					document.getElementById("content-box-4").style.left = "0px"
 				}
 			}, 100);
